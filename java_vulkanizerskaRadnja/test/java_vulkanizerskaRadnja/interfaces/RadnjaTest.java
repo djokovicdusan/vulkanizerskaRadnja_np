ced154853ee4ad13c75dde6c314444e442946fe8
djokovicdusan <ddjokovic9@gmail.com>
2022-03-20 19:11:14 +0100

    Naknadno dodat jos jedan test case za razlicite dimenzija

--- java_vulkanizerskaRadnja/test/java_vulkanizerskaRadnja/interfaces/RadnjaTest.java
+++ java_vulkanizerskaRadnja/test/java_vulkanizerskaRadnja/interfaces/RadnjaTest.java
@@ -49,6 +49,18 @@ class RadnjaTest {
 		assertTrue(!r.pronadjiGumu("Tigar").contains(ag2));
 	}
 	@Test
+	void testPronadjiGumuDveGumeRazlicitihDimenzija() {
+		AutoGuma ag1 = new AutoGuma("Tigar", 20, 210, 50);
+		AutoGuma ag2 = new AutoGuma("Tigar", 19, 210, 50);
+		
+		r.dodajGumu(ag1);
+		r.dodajGumu(ag2);
+		
+		assertEquals(2, r.pronadjiGumu("Tigar").size());
+		assertTrue(r.pronadjiGumu("Tigar").contains(ag1));
+		assertTrue(r.pronadjiGumu("Tigar").contains(ag2));
+	}
+	@Test
 	void testPronadjiGumuNePostojiMarka() {
 		AutoGuma ag1 = new AutoGuma("Tigar", 20, 210, 50);
 		AutoGuma ag2 = new AutoGuma("Michelin", 20, 210, 50);
